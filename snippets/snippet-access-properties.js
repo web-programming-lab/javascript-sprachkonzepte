3cdb6dbf937bb553d6e24ba0e76efd3a7c92fa13
Patrick Roos <pa.roos@icloud.com>
2023-01-15 16:43:48 +0100

    Destructuring assignment hinzugefügt

--- snippets/snippet-access-properties.js
+++ snippets/snippet-access-properties.js
@@ -4,11 +4,15 @@ const user = {
   name: 'patrick',
   age: 34,
 };
+
 console.log(user.name); // Patrick
 console.log(user['name']); // Patrick
 
+const { name, age } = user; // Destructuring assignment
+console.log(name, age); // Patrick 34
+
 // Computed Properties
-let fruit = prompt('Which fruit?', 'apple');
+let fruit = 'apple';
 let bag = {
   [fruit]: 5,
 };
