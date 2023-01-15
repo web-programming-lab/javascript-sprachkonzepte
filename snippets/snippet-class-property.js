da3ebd1c6da45cfa8c40c129a7c5f8e9ee3d3fa7
Patrick Roos <pa.roos@icloud.com>
2023-01-15 17:26:55 +0100

    Private class fields hinzugefügt

--- snippets/snippet-class-property.js
+++ snippets/snippet-class-property.js
@@ -1,8 +1,11 @@
 /* eslint-disable no-unused-vars */
 
 class Person {
-  constructor(name) {
+  #age; // Private class fields
+  _name = '';
+  constructor(name, age) {
     this._name = name;
+    this.#age = age;
   }
   get name() {
     return this._name;
@@ -10,4 +13,14 @@ class Person {
   set name(value) {
     this._name = value;
   }
+
+  get age() {
+    return this.#age;
+  }
 }
+
+const person = new Person('Patrick Roos', 35);
+person.name = 'Andreas Schürmann';
+console.log(person.name);
+
+person.age = 34; // TypeError: Cannot set property age of #<Person> which has only a getter
