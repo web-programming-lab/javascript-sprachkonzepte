d189cf4a906a2010e9bae77ccd2c4889021fbfa2
Patrick Roos <pa.roos@icloud.com>
2023-01-18 21:05:23 +0100

    Move Destructuring assignment example to snippet-array

--- snippets/snippet-array.js
+++ snippets/snippet-array.js
@@ -10,3 +10,7 @@ console.log(myArray); // ['x', 'y'];
 myArray.push(...['z', '1']);
 console.log(myArray); // ['x', 'y', 'z', '1'];
 console.log(typeof myArray); // object
+
+let a, bc;
+[a, ...bc] = myArray;
+console.log(a, bc); // 'a' ['b', 'c']
