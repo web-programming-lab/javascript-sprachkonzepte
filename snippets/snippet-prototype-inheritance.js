22e5949453ee9fdeb6395e85dd473ce4ef0836fd
Patrick Roos <pa.roos@icloud.com>
2023-01-24 20:53:12 +0100

    Prototype comments

--- snippets/snippet-prototype-inheritance.js
+++ snippets/snippet-prototype-inheritance.js
@@ -14,3 +14,11 @@ console.log(Object.getPrototypeOf(car) === vehicle); //Output: true
 const car2 = Object.create(vehicle, { hasFourWheels: { value: true } }); // Property descriptor
 console.log(car2.hasEngine);
 console.log(car2.hasFourWheels);
+
+//
+// Prototype chain
+// car2.__proto__ ==> vehicle, vehicle.__proto__ ==> Object.prototype, Object.prototype.__proto__ ==> null
+//
+console.log(car2.__proto__ === vehicle); //Output: true
+console.log(vehicle.__proto__ === Object.prototype); //Output: true
+console.log(Object.prototype.__proto__ === null); //Output: true
