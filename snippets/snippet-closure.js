507ac378648ecb010e46107bc870191b949f6833
Patrick Roos <pa.roos@icloud.com>
2023-01-24 20:50:17 +0100

    Add 'IIFE' comment

--- snippets/snippet-closure.js
+++ snippets/snippet-closure.js
@@ -4,7 +4,7 @@ let add = (function () {
     counter += 1;
     return counter;
   };
-})();
+})(); // IIFE (Immediately Invoked Function Expression) or Self-Executing Anonymous Function
 console.log(add()); //1
 console.log(add()); //2
 console.log(add()); //3
