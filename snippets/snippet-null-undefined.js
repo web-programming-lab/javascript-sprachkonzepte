13dfebe266f80c1d6bdae71ab3dd5e2cf42a97f1
Patrick Roos <pa.roos@icloud.com>
2023-01-24 19:28:17 +0100

    nullish coalescing operator added

--- snippets/snippet-null-undefined.js
+++ snippets/snippet-null-undefined.js
@@ -2,3 +2,8 @@ const myNullValue = null;
 const myUndefinedValue = undefined;
 console.log(myNullValue); // null
 console.log(myUndefinedValue); // undefined
+
+// Nullish coalescing operator (ES2020)
+// Der Nullwert-Koaleszenz-Operator (??) ist ein logischer Operator, der seinen rechten Operanden zurückgibt, wenn sein linker Operand null oder undefined ist, und ansonsten seinen linken Operanden zurückgibt.
+console.log(myNullValue ?? 'example-value'); // example-value
+console.log(myUndefinedValue ?? 'example-value'); // example-value
