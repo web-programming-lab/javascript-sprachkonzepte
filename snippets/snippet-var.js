89a7a9960e7d8ed21486a9a7da267b0382f8dfc5
p18750 <david.lichtsteiner@css.ch>
2024-12-30 20:45:47 +0100

    edit var snippet

--- snippets/snippet-var.js
+++ snippets/snippet-var.js
@@ -4,7 +4,7 @@
 function varTest() {
   var x = 31;
   if (true) {
-    var x = 71; // gleiche Variable x, wird nach oben gueltig!
+    var x = 71; // gleiche Variable x, wird nach oben gueltig! Warning Duplicate declaration
     var y = 80;
     console.log(x); // 71
   }
